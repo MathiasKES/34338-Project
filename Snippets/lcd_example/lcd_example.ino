27d84511f78c1a230f4a94e3f10e768f31e5047c
MathiasKES <warlour@eksworld.dk>
2026-01-13 18:05:44 +0100

    Working LCD Example

--- Snippets/lcd_example/lcd_example.ino
+++ Snippets/lcd_example/lcd_example.ino
@@ -1,6 +1,12 @@
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd{0x27, 16, 2};
+#define SS_PIN 10
+#define RST_PIN 9
+#define LCD_COLUMNS 16
+#define LCD_ROWS 2
+#define LCD_ADDRESS 0x27
+
+LiquidCrystal_I2C lcd{LCD_ADDRESS, LCD_COLUMNS, LCD_ROWS};
 
 /*
  * @brief Arduino setup function
@@ -14,11 +20,29 @@ void setup() {
   lcd.print("  Hello world!  ");
 }
 
+uint64_t counter = 0;
 
 /*
  * @brief Main Arduino loop
  */
 void loop() {
+  lcd_update(counter);
 
-  delay(500);
+  counter++;
+  if (counter > 9999999999999999ULL) counter = 0;
+}
+
+
+void lcd_update(uint64_t v)
+{
+  char buf[17] = "                "; // 16 spaces + '\0'
+
+  int i = 15;
+  do {
+    buf[i--] = '0' + (v % 10);
+    v /= 10;
+  } while (v && i >= 0);
+
+  lcd.setCursor(0, 1);
+  lcd.print(buf);
 }
