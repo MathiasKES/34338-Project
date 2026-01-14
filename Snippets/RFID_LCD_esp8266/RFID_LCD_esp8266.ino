b36634ee7785365caa31c7f2dcb4e379080dff32
MathiasKES <warlour@eksworld.dk>
2026-01-14 12:27:59 +0100

    Starter code for servo

--- Snippets/RFID_LCD_esp8266/RFID_LCD_esp8266.ino
+++ Snippets/RFID_LCD_esp8266/RFID_LCD_esp8266.ino
@@ -2,6 +2,7 @@
 #include <MFRC522.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <Servo.h>
 
 #define SDA_pin 15 // SDA, GPIO15, D8
 #define RST_PIN 16 // RST, GPIO16, D0 
@@ -10,8 +11,11 @@
 #define LCD_ROWS 2
 #define LCD_ADDRESS 0x27
 
+#define SERVO_PIN 0 // GPIO0, D3
+
 MFRC522 mfrc522(SDA_pin, RST_PIN);
 LiquidCrystal_I2C lcd(LCD_ADDRESS, LCD_COLUMNS, LCD_ROWS);
+Servo myservo;
 
 void setup() {
   Serial.begin(9600);
@@ -28,10 +32,33 @@ void setup() {
   SPI.begin();
   mfrc522.PCD_Init();
 
+  myservo.attach(SERVO_PIN);
+  myservo.write(0);
+
   Serial.println("RC522 initialized");
 }
 
+bool accessGranted = false;
+bool turning = false;
+uint16_t timer = 0;
+bool displaying = false;
+
+
 void loop() {
+  if (turning) {
+    Serial.println(myservo.read());
+    if (myservo.read() < 180) {
+      myservo.write(180);
+    } else {
+      myservo.write(0);
+
+      // Reset
+      turning = false;
+      lcd.setCursor(0, 0);
+      lcd.print("Scan RFID card  ");
+    }
+  }
+
   if (!mfrc522.PICC_IsNewCardPresent()) return;
   if (!mfrc522.PICC_ReadCardSerial()) return;
 
@@ -54,7 +81,6 @@ void loop() {
   Serial.println();
 
   // -------- UID CHECK --------
-  bool accessGranted = false;
 
   if (mfrc522.uid.size == 4 &&
       mfrc522.uid.uidByte[0] == 0xE3 &&
@@ -62,20 +88,36 @@ void loop() {
       mfrc522.uid.uidByte[2] == 0x0F &&
       mfrc522.uid.uidByte[3] == 0xDA) {
     accessGranted = true;
+    Serial.println("setting accessgranted");
   }
 
   // Show result
   lcd.setCursor(0, 0);
   if (accessGranted) {
-    lcd.print("Access granted ");
-  } else {
-    lcd.print("Access denied  ");
+    accessGranted = false;
+    turning = true;
+    lcd.print("Access granted  ");
+    displaying = true;
+
+  } else if (!accessGranted && !turning) {
+    lcd.print("Access denied   ");
+    displaying = true;
+  }
+
+  if (displaying) {
+    timer++;
+    if (timer >= 120) {
+      displaying = false;
+      timer = 0;
+      lcd.setCursor(0, 0);
+      lcd.print("                ");
+    }
   }
 
   // Halt card and stop encryption
   mfrc522.PICC_HaltA();
   mfrc522.PCD_StopCrypto1();
 
-  delay(1000);
+  // delay(1000);
 }
 
